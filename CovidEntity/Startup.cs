eeda31e9be59cd7d10fbfc1961ac3639821d610d
Pramod Kumar <pramod6507@gmail.com>
2020-09-28 18:51:49 -0400

    Add project files.

---
+++ CovidEntity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CovidEntity.Startup))]
+namespace CovidEntity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
